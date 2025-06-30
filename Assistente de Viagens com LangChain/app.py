d6aab3a7911172c8bdd131c9b2192079825194eb
Vinícius Berto Quintino <viniciusbertoquintino@gmail.com>
2025-06-30 08:38:42 -0300

    .

--- Assistente de Viagens com LangChain/app.py
+++ Assistente de Viagens com LangChain/app.py
@@ -22,3 +22,41 @@ prompt = ChatPromptTemplate.from_messages([
     MessagesPlaceholder(variable_name="history"),  # Placeholder para o histórico de mensagens
     ("human", "{input}")  # Entrada do usuário
 ])
+
+llm = ChatOpenAI(temperature=0.7,model="gpt-4o-mini")  # Cria uma instância do modelo de linguagem com temperatura ajustada sendo 0.7 e modelo especificado como gpt-4o-mini.
+
+chain = prompt | llm  # Cria uma cadeia de execução que combina o template do prompt com o modelo de linguagem
+
+store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    """Retorna o histórico de mensagens para uma sessão específica."""
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()  # Cria um novo histórico se não existir
+    return store[session_id]
+
+chain_with_history = RunnableWithMessageHistory(
+    chain, 
+    get_session_history, # Função para obter o histórico de mensagens da sessão
+    input_messages_key="input",  # Define a chave para as mensagens de entrada
+    history_messages_key="history"  # Define a chave para as mensagens de histórico
+)
+
+def iniciar_assistente_viagem():
+    print("Bem-vindo ao Assistente de Viagens! Digite 'Sair' para encerrar a conversa.\n")
+    while True:
+        pergunta_usuario = input("Você: ")
+
+        if pergunta_usuario.lower() in ["sair", "exit"]:
+            print("Assistente de Viagem: Até mais! Aproveite sua viagem!")
+            break
+
+        resposta = chain_with_history.invoke(
+            {"input": pergunta_usuario},
+            config={'configurable': {'session_id': 'user123'}}  # Exemplo de ID de sessão, pode ser dinâmico
+        )
+
+        print("Assistente de viagem:", resposta.content)
+
+if __name__ == "__main__":
+    iniciar_assistente_viagem()  # Inicia o assistente de viagem
